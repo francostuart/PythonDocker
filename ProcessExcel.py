d3305ea45ea5866e3d8dafa0704787016f495bcf
ricardo <rbalbis2001@gmail.com>
2025-06-22 20:47:05 -0500

    se modifica el archivo ProcessExcel.py ahora se solicitan los argumentos por consola

--- ProcessExcel.py
+++ ProcessExcel.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime, time as dt_time
 import pandas as pd
+import argparse
 
 
 def tu_script_principal(ruta=""):
@@ -11,7 +12,7 @@ def tu_script_principal(ruta=""):
     print(df)
 
 
-def ejecutar_en_horario(hora_inicio, hora_fin, intervalo=3600):
+def ejecutar_en_horario(input, hora_inicio, hora_fin, intervalo=3600):
     """
     Ejecuta un script dentro de un rango horario específico
 
@@ -30,7 +31,7 @@ def ejecutar_en_horario(hora_inicio, hora_fin, intervalo=3600):
         # Verificar si estamos dentro del horario deseado
         if hora_inicio <= ahora <= hora_fin:
             print(f"Ejecutando script a las {ahora}")
-            tu_script_principal(ruta=r"input.xlsx")
+            tu_script_principal(ruta=input)
         else:
             print(f"Fuera del horario programado. Hora actual: {ahora}")
 
@@ -40,8 +41,27 @@ def ejecutar_en_horario(hora_inicio, hora_fin, intervalo=3600):
 
 # Ejemplo: ejecutar entre las 9:30 y las 18:15, verificando cada 5 minutos
 if __name__ == "__main__":
-    ejecutar_en_horario(
-        hora_inicio=(5, 30),  # 9:30 AM
-        hora_fin=(23, 50),  # 6:15 PM
-        intervalo=100  # 5 minutos (300 segundos)
-    )
+    parser = argparse.ArgumentParser(description="Script para procesar datos con Pandas.")
+
+    # Definir argumentos
+    parser.add_argument("input", help="Ruta al archivo de entrada (ej. input.xlsx)")
+    parser.add_argument("start", help="hora de inicio de ejecucion del script - 9:30 AM")
+    parser.add_argument("end", help="hora de fin de ejecucion del script - 6:30 PM")
+    parser.add_argument("interval", help="intervalo de ejecucion del script - 5 minutos (300 segundos)")
+
+    # Parsear los argumentos de la línea de comandos
+    args = parser.parse_args()
+
+    # Convertir los argumentos a los formatos adecuados
+    hora_inicio = tuple(map(int, args.start.split(':')))  # Convierte "9:30" a (9, 30)
+    hora_fin = tuple(map(int, args.end.split(':')))  # Convierte "18:30" a (18, 30)
+    intervalo = int(args.interval)  # Convierte el intervalo a entero
+
+    # Llamar a la función principal con los argumentos obtenidos
+    # print(args.input, args.start, args.end, args.interval)
+    ejecutar_en_horario(input=args.input, hora_inicio=hora_inicio, hora_fin=hora_fin, intervalo=intervalo)
+    # ejecutar_en_horario(
+    #     hora_inicio=(5, 30),  # 9:30 AM
+    #     hora_fin=(23, 50),  # 6:15 PM
+    #     intervalo=100  # 5 minutos (300 segundos)
+    # )
